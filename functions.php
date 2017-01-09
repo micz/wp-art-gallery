d497e0c4c7d68a317191848037fae747c789b9e5
mic <m@micz.it>
2017-01-09 23:18:30 +0100

    license added

--- functions.php
+++ functions.php
@@ -1,4 +1,22 @@
 <?php
+/* Copyright 2016 Mic (email: m@micz.it)
+Plugin Info: http://micz.it/wordpress-plugin-art-gallery/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+
 function wpmz_get_attachment( $attachment_id ) {
 	$attachment = get_post( $attachment_id );
 	return array(
